f8c0f9fa05a3a49520660d731255f21c52701299
SC-MMO <moritz.mummert@scaltel.de>
2026-08-14 10:04:14 +0200

    fix: fixed schema errors

--- src/helpers/readFromSchema.ts
+++ src/helpers/readFromSchema.ts
@@ -1,34 +1,25 @@
 import * as Path from 'path';
-import { CanonicalCollectionSchema, type CanonicalCollection } from './types';
+import { CanonicalCollectionSchema, type CanonicalCollections } from './types';
 import fs from 'fs';
 import { readYaml } from './fileHelper.js';
+import { z } from 'zod';
 
-function readOneSchema(path: string): CanonicalCollection {
-	return CanonicalCollectionSchema.parse(readYaml(path));
+const CanonicalCollectionsSchema = z.record(z.string(), CanonicalCollectionSchema);
+
+function readOneSchema(path: string): CanonicalCollections {
+	return CanonicalCollectionsSchema.parse(readYaml(path));
 }
 
-function readMultipleSchemas(path: string): CanonicalCollection[] {
+function readMultipleSchemas(path: string): CanonicalCollections[] {
 	const files = fs.readdirSync(path, { encoding: 'utf8', recursive: true });
+
 	return files.filter((file) => file.endsWith('.yaml') || file.endsWith('.yml')).map((file) => readOneSchema(Path.join(path, file)));
 }
 
-export function readFromSchema(path: string, selectedCollections: string[] | null = null): Record<string, CanonicalCollection> {
+export function readFromSchema(path: string, selectedCollections: string[] | null = null): CanonicalCollections {
 	const isFile = fs.statSync(path).isFile();
 
-	const collections = isFile ? readOneSchema(path) : readMultipleSchemas(path).reduce((acc, obj) => ({ ...acc, ...obj }), {});
+	const collections = isFile ? readOneSchema(path) : readMultipleSchemas(path).reduce<CanonicalCollections>((acc, obj) => ({ ...acc, ...obj }), {});
 
-	const filteredCollections =
-		selectedCollections === null ? collections : Object.fromEntries(Object.entries(collections).filter(([key]) => selectedCollections.includes(key)));
-
-	const collectionMap = Object.fromEntries(
-		Object.entries(filteredCollections).map(([key, value]) => {
-			const parsed = CanonicalCollectionSchema.safeParse(value);
-			if (!parsed.success) {
-				throw new Error(`Invalid schema for collection "${key}":\n${parsed.error.message}`);
-			}
-			return [key, parsed.data];
-		}),
-	);
-
-	return collectionMap;
+	return selectedCollections === null ? collections : Object.fromEntries(Object.entries(collections).filter(([key]) => selectedCollections.includes(key)));
 }
