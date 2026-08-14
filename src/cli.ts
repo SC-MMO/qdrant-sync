ea2aa6af1f6e46db1a1e4d3a4778630f8187b5f9
SC-MMO <moritz.mummert@scaltel.de>
2026-08-14 10:26:55 +0200

    feat: added push confirmation

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-
 import { runInit } from './commands/init';
 import { runPull } from './commands/pull';
 import { runPush } from './commands/push';
 import { runSnap } from './commands/snap';
 import { Command } from 'commander';
+import { confirm } from './helpers/confirm';
 
 const program = new Command();
 
@@ -19,25 +19,33 @@ program
 	.action(() => {
 		runInit();
 	});
+
 program
 	.command('pull')
 	.description('Pull your Qdrant configuration into a schema')
 	.action(() => {
 		void runPull();
 	});
+
 program
 	.command('snap')
 	.description('Create a snap of your schema')
 	.argument('<name>', 'Name of the snap')
-	.action((args: { name: string }) => {
-		void runSnap(args.name);
+	.action((name: string) => {
+		void runSnap(name);
 	});
+
 program
 	.command('push')
 	.description('Push a schema or snap to Qdrant')
 	.argument('[source]', 'Source of the schema to be pushed')
-	.action((args: { source?: string }) => {
-		void (args.source ? runPush(args.source) : runPush());
+	.option('-f, --force', 'Pushes without asking for user validation')
+	.action(async (source: string | undefined, options: { force?: boolean }) => {
+		const approved = options.force === true ? true : await confirm('This will overwrite your database configuration, do you want that?');
+
+		if (approved) {
+			await (source ? runPush(source) : runPush());
+		}
 	});
 
 program.parse();
