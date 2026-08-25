7c1fe7b34012bc71ce102c54b864cec689298865
SC-MMO <moritz.mummert@scaltel.de>
2026-08-25 09:53:49 +0200

    feat: simplified snap name

--- src/helpers/snapName.ts
+++ src/helpers/snapName.ts
@@ -2,16 +2,7 @@ import { randomUUID } from 'crypto';
 
 export function snapName(name: string): string {
 	const now = new Date();
-
-	const timestamp =
-		now.getFullYear().toString() +
-		String(now.getMonth() + 1).padStart(2, '0') +
-		String(now.getDate()).padStart(2, '0') +
-		String(now.getHours()).padStart(2, '0') +
-		String(now.getMinutes()).padStart(2, '0') +
-		String(now.getSeconds()).padStart(2, '0');
-
 	const uuid = randomUUID().replace(/-/g, '');
 
-	return `${timestamp}_${name}_${uuid}.yaml`;
+	return `${now.toISOString()}_${name}_${uuid}.yaml`;
 }
